22ae26f3806791848c5bdb31e119ed3b7bba91bb
YangChao925 <1758584523@qq.com>
2019-12-02 10:56:25 +0800

    对启动类进行修改

--- src/main/java/com/qf/SpringbootApp.java
+++ src/main/java/com/qf/SpringbootApp.java
@@ -10,6 +10,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class SpringbootApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        System.out.println("这是第一次测试pull,push");//代码改动后首先pull拉取git上面最新的代码，避免其他用户已经修改过了git上面的代码，直接提交会覆盖修改后的代码。
+                                                            //之后在点击commit changs----进行标识更改
+
         SpringApplication.run(SpringbootApp.class);
     }
 
